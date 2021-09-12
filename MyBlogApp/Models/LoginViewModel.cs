f1f3e6f148728627058ca61f3914db44f9f38fba
NonSuger444 <crowrabbit1212@gmail.com>
2021-09-12 23:50:17 +0900

    ログイン画面作成

---
+++ MyBlogApp/Models/LoginViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace MyBlogApp.Models
+{
+    /// <summary>
+    /// LoginViewModel
+    /// </summary>
+    public class LoginViewModel
+    {
+        /// <summary>
+        /// ユーザー名
+        /// </summary>
+        [Required]
+        [DisplayName("ユーザー名")]
+        public string UserName { get; set; }
+        /// <summary>
+        /// パスワード
+        /// </summary>
+        [Required]
+        [DisplayName("パスワード")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
